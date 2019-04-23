2592503f4426f54c34ef84cf0d4e7825c5d20924
CarsonBelew <carbelew@gmail.com>
2019-04-23 12:17:07 -0500

    mouse events exercise

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -14,10 +14,32 @@
 // // selecting class
 // $(".codeup").css("border", "1px solid red");
 
-// element and multiple selectors
-$("li").css("font-size", "20px");
+// // element and multiple selectors
+// $("li").css("font-size", "20px");
+//
+// $("h1, li, p").css("background-color", "yellow");
+//
+//
+// alert($("h1").html());
 
-$("h1, li, p").css("background-color", "#fff2a8");
 
+// *****MOUSE EVENTS*****
 
-alert($("h1").html());
+// change the background of a h1 when clicked
+$("h1").click(function(){
+    $("h1").css("background", "blue");
+});
+
+// all <p> have a font-size of 18px when dbl clicked
+$("p").dblclick(function(){
+    $("p").css("font-size", "18px");
+});
+
+// all <li> text red when mouse hovers and resets to black when not hover
+$("li").hover(function() {
+    $(this).css("color", "red")
+},
+    function () {
+        $(this).css("color", "black")
+    }
+);
