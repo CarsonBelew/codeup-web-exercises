b03597d003f7f708ec2142dbf439e04f5e8ca566
CarsonBelew <carbelew@gmail.com>
2019-04-23 11:15:13 -0500

    exercises

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -11,5 +11,13 @@
 // // alerting a second id
 // alert($("#para").html());
 
+// // selecting class
+// $(".codeup").css("border", "1px solid red");
 
-$(".codeup").css("border", "1px solid red");
+// element and multiple selectors
+$("li").css("font-size", "20px");
+
+$("h1, li, p").css("background-color", "#fff2a8");
+
+
+alert($("h1").html());
