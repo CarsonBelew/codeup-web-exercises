0381b383ba84c3c2dd4d36d21b2103830eb2b26d
CarsonBelew <carbelew@gmail.com>
2019-05-06 14:09:39 -0500

    played around with trying to log a pending notice

--- js/promises.js
+++ js/promises.js
@@ -1,10 +1,10 @@
 "use strict";
 
-const wait = (number) => {
+const wait = (num) => {
     return new Promise((resolve, reject) => {
-        setTimeout(() => resolve(), number);
+        setTimeout(() => resolve(), num);
     });
 };
 
-wait(1000).then(() => console.log('You\'ll see this after 1 second'));
-wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
+wait(1000).then(() => console.log("You'll see this after 1 second"));
+wait(3000).then(() => console.log("You'll see this after 3 seconds"));
