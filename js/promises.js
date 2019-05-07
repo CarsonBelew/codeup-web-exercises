14c67544a047d15e404e4c260cafc999e5a7189a
CarsonBelew <carbelew@gmail.com>
2019-05-07 09:33:54 -0500

    changed const name for better readability

--- js/promises.js
+++ js/promises.js
@@ -13,8 +13,8 @@ wait(3000).then(() => console.log("You'll see this after 3 seconds"));
 
 const dateOfLastCommit = username => {
     const url = `https://api.github.com/users/${username}/events`;
-    const options = {headers: {'Authorization': gitHubKey}};
-    return fetch(url, options)
+    const event = {headers: {'Authorization': gitHubKey}};
+    return fetch(url, event)
         .then(response => response.json())
         .then(data => data[0].created_at);
 };
