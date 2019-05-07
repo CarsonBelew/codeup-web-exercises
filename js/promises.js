7061364d8b577fe462c96086028f97c66f428e76
CarsonBelew <carbelew@gmail.com>
2019-05-07 09:15:38 -0500

    promises exercise second part

--- js/promises.js
+++ js/promises.js
@@ -10,3 +10,13 @@ const wait = (num) => {
 
 wait(1000).then(() => console.log("You'll see this after 1 second"));
 wait(3000).then(() => console.log("You'll see this after 3 seconds"));
+
+const dateOfLastCommit = username => {
+    const url = `https://api.github.com/users/${username}/events`;
+    const options = {headers: {'Authorization': gitHubKey}};
+    return fetch(url, options)
+        .then(response => response.json())
+        .then(data => data[0].created_at);
+};
+console.log(dateOfLastCommit('CarsonBelew').then(lastCommitDate =>
+    console.log(lastCommitDate)));
